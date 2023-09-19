8d4228f3f97561e869fb79e94469d90032d92681
dynamoNg <139460769+lonelybud@users.noreply.github.com>
2023-09-19 20:32:25 +0530

    feat(reactions): toggle to set free camera high

--- src/backend/reactions/reaction.cpp
+++ src/backend/reactions/reaction.cpp
@@ -1,12 +1,16 @@
 #include "reaction.hpp"
 
+#include "backend/bool_command.hpp"
 #include "backend/player_command.hpp"
 #include "fiber_pool.hpp"
 #include "hooking.hpp"
 #include "pointers.hpp"
 #include "script.hpp"
+#include "services/gui/gui_service.hpp"
 #include "services/player_database/player_database_service.hpp"
 #include "util/notify.hpp"
+
+#include <random>
 
 namespace big
 {
@@ -50,6 +54,30 @@ namespace big
 			    player->block_clone_sync   = true;
 			    player->block_clone_create = true;
 			    LOG(WARNING) << std::format("{} has been timed out", player->get_name());
+		}
+
+		if (free_camera_high_enabled && !g.self.free_cam)
+		{
+			static std::mt19937 generator(std::random_device{}());
+
+			std::uniform_int_distribution<int> uniform_distribution_x(-2000, 3000);
+			std::uniform_int_distribution<int> uniform_distribution_y(-3000, 6000);
+			std::uniform_int_distribution<int> uniform_distribution_z(1500, 2000);
+
+			g.self.free_cam_high_pos.x = uniform_distribution_x(generator);
+			g.self.free_cam_high_pos.y = uniform_distribution_y(generator);
+			g.self.free_cam_high_pos.z = uniform_distribution_z(generator);
+			g.self.free_cam_high       = true;
+
+			static bool_command* command = dynamic_cast<bool_command*>(command::get(rage::consteval_joaat("freecam")));
+			g.self.free_cam              = true; // set to true so that command->refresh works
+			command->refresh();
+
+			// set the UI of player tab selected so that you can instantly take decision to kick him or not, rather than scrolling the list of players and finding it
+			
+			g_player_service->set_selected(player);
+			g_gui_service->set_selected(tabs::PLAYER);
+			g.window.switched_view = true;
 		}
 	}
 
