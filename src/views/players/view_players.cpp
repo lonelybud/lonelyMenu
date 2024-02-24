fb74cd60658442cf8c4cac504193b62ae276ace5
dynamoNg <139460769+lonelybud@users.noreply.github.com>
2024-02-24 18:46:15 +0530

    refractor code
    - dont auto kick when blocked player joins
    - refractor view_player code for clarity
    - rename one file

--- src/views/players/view_players.cpp
+++ src/views/players/view_players.cpp
@@ -101,9 +101,11 @@ namespace big
 
 		if (!*g_pointers->m_gta.m_is_session_started && player_count < 2)
 			return;
+
+		const auto style = ImGui::GetStyle();
 		float window_pos = 110.f + g_gui_service->nav_ctr * ImGui::CalcTextSize("W").y
-		    + g_gui_service->nav_ctr * ImGui::GetStyle().ItemSpacing.y
-		    + g_gui_service->nav_ctr * ImGui::GetStyle().ItemInnerSpacing.y + ImGui::GetStyle().WindowPadding.y;
+		    + g_gui_service->nav_ctr * style.ItemSpacing.y + g_gui_service->nav_ctr * style.ItemInnerSpacing.y
+		    + style.WindowPadding.y;
 
 		ImGui::SetNextWindowSize({300.f, 0.f});
 		ImGui::SetNextWindowPos({10.f, window_pos});
@@ -111,31 +113,30 @@ namespace big
 
 		if (ImGui::Begin("playerlist", nullptr, window_flags))
 		{
+			ImGui::PushStyleColor(ImGuiCol_FrameBg, {0.f, 0.f, 0.f, 0.f});
+			ImGui::PushStyleColor(ImGuiCol_ScrollbarBg, {0.f, 0.f, 0.f, 0.f});
+			
 			const auto style = ImGui::GetStyle();
-			float window_height = (ImGui::CalcTextSize("A").y + style.FramePadding.y * 2.0f + style.ItemSpacing.y) // button size
+			auto window_width = ImGui::GetWindowSize().x - style.WindowPadding.x * 2;
+
+			// render search
+			static std::string search_player_name;
+			ImGui::SetNextItemWidth(window_width);
+			if (components::input_text_with_hint("###search_player_name", "search name", search_player_name))
+				std::transform(search_player_name.begin(), search_player_name.end(), search_player_name.begin(), ::tolower);
+			auto search_height = ImGui::GetItemRectSize().y; // Get the size of the last element
+			//
+
+			float raw_height = (ImGui::CalcTextSize("A").y + style.FramePadding.y * 2.0f + style.ItemSpacing.y) // button size
 			        * player_count                                       // amount of players
 			    + (player_count > 1) * ((style.ItemSpacing.y * 2) + 1.f) // account for ImGui::Separator spacing
 			    + (player_count == 1) * 2.f;                             // some arbitrary height to make it fit
 			// used to account for scrollbar width
-			has_scrollbar = window_height + window_pos > (float)*g_pointers->m_gta.m_resolution_y - 10.f;
+			has_scrollbar = raw_height + window_pos > (float)*g_pointers->m_gta.m_resolution_y - 10.f;
+			// when scrollbar is there compute available space otherwise use raw height
+			float window_height = has_scrollbar ? (float)*g_pointers->m_gta.m_resolution_y - (window_pos + search_height + 40.f) : raw_height;
 
-			ImGui::PushStyleColor(ImGuiCol_FrameBg, {0.f, 0.f, 0.f, 0.f});
-			ImGui::PushStyleColor(ImGuiCol_ScrollbarBg, {0.f, 0.f, 0.f, 0.f});
-
-			auto width_of_player_list = ImGui::GetWindowSize().x - ImGui::GetStyle().WindowPadding.x * 2;
-
-			static std::string search_player_name;
-			ImGui::SetNextItemWidth(width_of_player_list);
-			if (components::input_text_with_hint("###search_player_name", "search name", search_player_name))
-				std::transform(search_player_name.begin(), search_player_name.end(), search_player_name.begin(), ::tolower);
-
-			auto height_of_search_input = ImGui::GetItemRectSize().y; // Get the size of the last element
-
-			// basically whichever is smaller, the max available screenspace or the calculated window_height
-			window_height = has_scrollbar ? (float)*g_pointers->m_gta.m_resolution_y - (window_pos + 40.f) : window_height;
-			window_height -= height_of_search_input; // subtract height of search_input
-
-			if (ImGui::BeginListBox("##players", {width_of_player_list, window_height}))
+			if (ImGui::BeginListBox("##players", {window_width, window_height}))
 			{
 				ImGui::SetScrollX(0);
 				player_button(g_player_service->get_self());
